5d618b84cd09129363263165947fd6fec58a1457
IznaDev <oussama.boukhrass@gmail.com>
2025-02-11 15:40:31 +0100

    ping function modification

--- src/scanner/net.rs
+++ src/scanner/net.rs
@@ -1,10 +1,13 @@
-use tokio::net::TcpStream;
+use std::io::Error;
+
+//use tokio::net::TcpStream;
+use tokio;
 
 // to ping a port of a host. it is an asynchronous function
-pub async fn tcp_ping(host: &str, port: u16) -> bool {
+pub async fn tcp_ping(host: &str, port: u16) -> Result<bool, Error> {
     let address = (host, port);
-    match TcpStream::connect(address).await {
-        Ok(_) => true,
-        Err(_) => false,
+    match tokio::net::lookup_host(address).await {
+        Ok(boolean) => Ok(boolean),
+        Err(e) => Err(e),
     }
 }
